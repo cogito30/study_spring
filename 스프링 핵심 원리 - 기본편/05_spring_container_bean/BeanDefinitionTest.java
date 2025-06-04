d5e2e093527d0809dfd4928465892651f81d2cee
cogito30 <cogito30g@gmail.com>
2025-06-04 16:40:49 +0900

    docs: (스프링 핵심 원리 - 기본편) 05 스프링 컨테이너와 스프링 빈

--- 스프링 핵심 원리 - 기본편/05_spring_container_bean/BeanDefinitionTest.java
+++ 스프링 핵심 원리 - 기본편/05_spring_container_bean/BeanDefinitionTest.java
@@ -2,6 +2,7 @@ package hello.core.beandefinition;
 
 public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
